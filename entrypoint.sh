0cfabcc2639881793e11d22b21ba9d8f3189c28e
prab-gitcode <prabhakarkoppisetty@gmail.com>
2025-10-07 09:36:28 +0100

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,15 @@ fi
 JM_SCENARIOS=${JM_HOME}/scenarios
 JM_REPORTS=${JM_HOME}/reports
 JM_LOGS=${JM_HOME}/logs
+JM_DATA=${JM_HOME}/data
 
 mkdir -p ${JM_REPORTS} ${JM_LOGS}
 
 SCENARIOFILE=${JM_SCENARIOS}/${TEST_SCENARIO}.jmx
 REPORTFILE=${NOW}-perftest-${TEST_SCENARIO}-report.csv
 LOGFILE=${JM_LOGS}/perftest-${TEST_SCENARIO}.log
+
+echo "data path: ${JM_DATA}"
 
 # Run the test suite
 jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jenv="${ENVIRONMENT}"
